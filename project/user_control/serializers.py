a7b612d699ff782ca0a1015d31a3ef4ea2029e90
Santiago1916 <santiago.abadia.gomez01@gmail.com>
2022-04-25 13:18:12 -0500

    serializers

--- project/user_control/serializers.py
+++ project/user_control/serializers.py
@@ -2,14 +2,14 @@ from rest_framework import serializers
 
 
 class LoginSerializer(serializers.Serializer):
-    email = serializers.EmailField()
+    username = serializers.CharField()
     password = serializers.CharField()
 
 
 class RegisterSerializer(serializers.Serializer):
-    email = serializers.EmailField()
+    username = serializers.CharField()
     password = serializers.CharField()
-    name = serializers.CharField()
+
 
 
 class RefreshSerializer(serializers.Serializer):
