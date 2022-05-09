5244835ca8c550e2db4494ecc215327c96452de9
etensor <dpenillac@correo.usbcali.edu.co>
2022-05-09 17:37:57 -0500

    django + postgres models wtf

---
+++ zapaapp/entidades/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+
+class Mensaje(models.Model):
+    contenido = models.TextField()
+    emisor = models.textField()
+    receptor = models.textField()
+    fecha = models.DateTimeField(auto_now_add=True)
+
+
+class Usuario(models.Model):
+    nombre = models.TextField()
+    email = models.TextField()
+    password = models.TextField()
+    fecha_registro = models.DateTimeField(auto_now_add=True)
+    fecha_ultimo_login = models.DateTimeField(auto_now=True)
+    estado = models.BooleanField(default=True)
+
+
+class Canal(models.Model):
+    nombre = models.TextField()
+    mensajes = models.ManyToManyField(Mensaje)
+
+
+    
