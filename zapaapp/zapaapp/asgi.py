b5e2e9f10122583c9c66cb2ba5c44d6279624f33
etensor <dpenillac@correo.usbcali.edu.co>
2022-05-16 16:21:33 -0400

    websocket : django channels para el chat + merger nuevas cosas ?

--- zapaapp/zapaapp/asgi.py
+++ zapaapp/zapaapp/asgi.py
@@ -8,9 +8,18 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 """
 
 import os
+import django
 
-from django.core.asgi import get_asgi_application
+#from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter # para el chat
+from channels.http import AsgiHandler # para el chat -> versiones en conflicto: "django >=2.2 no longere has inbuilt ASGI support..."
+# por suerte channels presenta una alternativa
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'zapaapp.settings')
+django.setup()
 
-application = get_asgi_application()
+
+#application = #get_asgi_application()
+application = ProtocolTypeRouter({
+    'http': AsgiHandler(),        #get_asgi_application(),
+})
