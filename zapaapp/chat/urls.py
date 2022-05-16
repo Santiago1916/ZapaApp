6ae17146641a808efd2107e6997a654bb0f2c3b3
Santiago1916 <santiago.abadia.gomez01@gmail.com>
2022-05-16 15:17:11 -0500

    urls and view vistas

--- zapaapp/chat/urls.py
+++ zapaapp/chat/urls.py
@@ -5,3 +5,15 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('<int:pk>/', views.msg_details, name='msg_details'),
 ]
+'''
+# path -> listings/urls.py
+
+from django.urls import path 
+from .import views
+
+app_name = 'listings'
+
+urlpatterns = [
+    path('login_page/', views.login_page, name='login_page'), 
+]
+'''
--- zapaapp/chat/views.py
+++ zapaapp/chat/views.py
@@ -5,20 +5,24 @@ from django.http import HttpResponse
 
 from chat.models import Mensaje, Usuario, Canal
 
-def index(request,pk):
-    #return HttpResponse('Hello, world. You are at the chat index.')
-    return msg_details(1)
+
+def index(request, pk):
+    # return HttpResponse('Hello, world. You are at the chat index.')
+    return msg_details( 1 )
 
 
-def msg_details(request,pk):
-    msg = Mensaje.objects.get(pk=pk)
-    emsr = Mensaje.objects.filter(emisor=msg.emisor)
-    rcpt = Mensaje.objects.filter(receptor=msg.receptor)
+def msg_details(request, pk):
+    msg = Mensaje.objects.get( pk=pk )
+    emsr = Mensaje.objects.filter( emisor=msg.emisor )
+    rcpt = Mensaje.objects.filter( receptor=msg.receptor )
 
     context = {
         'de': emsr,
         'para': rcpt,
     }
 
-    return render(request, 'chat/vistas/msg_details.html', context)
-    
+    return render( request, 'chat/vistas/msg_details.html', context )
+
+
+def login_page(request):
+    return render(request, 'listings/login_page.html')
