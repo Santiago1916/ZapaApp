424c9e8bac74e3679c5a6bcf50a4220179176852
etensor <dpenillac@correo.usbcali.edu.co>
2022-05-16 05:51:07 -0400

    admin.py list_display para ver mejor las cosas, nose si sirva de mas

--- zapaapp/chat/admin.py
+++ zapaapp/chat/admin.py
@@ -4,4 +4,8 @@ from chat.models import Canal, Mensaje, Usuario
 # Register your models here.
 admin.site.register(Canal)
 admin.site.register(Mensaje)
-admin.site.register(Usuario)
+#admin.site.register(Usuario)
+
+@admin.register(Usuario)
+class UsuarioAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'email', 'password', 'fecha_registro', 'fecha_ultimo_login', 'estado')
